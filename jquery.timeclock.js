c13a5ea4260e5c748e318df84e32dde7925aa101
Mikkel Hoegh <mikkel@hoegh.org>
2008-12-11 21:01:29 +0100

    Initial commit.

---
+++ jquery.timeclock.js
@@ -0,0 +1,8 @@
+/**
+ * jQuery Timeclock
+ *
+ * Copyright (c) 2008 Mikkel Hoegh <mikkel@hoegh.org>
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ */
