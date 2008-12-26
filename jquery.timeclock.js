7c17f4e583271869d10ef649d18bcf0a07e0b4d8
Jakob T. Svendsen <jakob@revealit.dk>
2008-12-26 16:41:24 +0100

    Added suport for initial setting of start time. Fixed indentation.

--- jquery.timeclock.js
+++ jquery.timeclock.js
@@ -16,7 +16,7 @@
 
  /* TimeClock contstructor */
 function TimeClock() {
-    this._defaults = {
+	this._defaults = {
 		// H: hours, M: minutes, S: seconds.
 		format: 'HM',
 		// Interval in miniseconds between updating this timeclock.
@@ -30,134 +30,146 @@ var PROP_NAME = 'timeclock';
 
 $.extend(TimeClock.prototype, {
 	// Class name added to indicate that an element is already
-    // configured with timeclock.
-    markerClassName: "hasTimeClock",
+	// configured with timeclock.
+	markerClassName: "hasTimeClock",
 
-    /**
-     * Attach a TimeClock to an element
-     *
-     * @param target element - the containing div-tag
-     * @param options object - initial settings for the countdown
-     */
+	/**
+	 * Attach a TimeClock to an element
+	 *
+	 * @param target element - the containing div-tag
+	 * @param options object - initial settings for the countdown
+	 */
 	_attachTimeClock: function(target, options) {
 		target = $(target);
-        if (target.hasClass(this.markerClassName)) {
-            return;
-        }
-        target.addClass(this.markerClassName);
-        if (!target[0].id) {
-            target[0].id = 'ticl' + new Date().getTime();
-        }
-        var inst = {};
-        inst.options = $.extend({}, options);
-        inst._origHTML = target.html();
-        this._adjustSettings(inst);
-        $.data(target[0], PROP_NAME, inst);
-        this._updateTimeClock(target, inst);
+		if (target.hasClass(this.markerClassName)) {
+			return;
+		}
+		target.addClass(this.markerClassName);
+		if (!target[0].id) {
+			target[0].id = 'ticl' + new Date().getTime();
+		}
+		var inst = {};
+		inst.options = $.extend({}, options);
+		inst._origHTML = target.html();
+		this._adjustSettings(inst);
+		console.log(inst);
+		$.data(target[0], PROP_NAME, inst);
+		this._updateTimeClock(target, inst);
 	},
 
-    /**
-     * Update the timeclock's display.
-     * @param id    element or string - the containing div-tag or its ID
-     * @param inst  object - the current settings for this instance.
-     */
-    _updateTimeClock: function(id, inst) {
-        var target = $(id);
-        inst = inst || $.data(target[0], PROP_NAME);
-        if (!inst) {
-            return;
-        }
-        target.html(this._generateHTML(inst));
-        target.toggleClass("tock");
-        var format = this._get(inst, 'format');
-        inst._timer = setTimeout('$.timeclock._updateTimeClock("#' + target[0].id + '")', this._get(inst, 'tick'));
-        $.data(target[0], PROP_NAME, inst);
-    },
+	/**
+	 * Update the timeclock's display.
+	 * @param id	element or string - the containing div-tag or its ID
+	 * @param inst  object - the current settings for this instance.
+	 */
+	_updateTimeClock: function(id, inst) {
+		var target = $(id);
+		inst = inst || $.data(target[0], PROP_NAME);
+		if (!inst) {
+			return;
+		}
+		target.html(this._generateHTML(inst));
+		target.toggleClass("tock");
+		var format = this._get(inst, 'format');
+		inst._timer = setTimeout('$.timeclock._updateTimeClock("#' + target[0].id + '")', this._get(inst, 'tick'));
+		$.data(target[0], PROP_NAME, inst);
+	},
 
-    /**
-     * Stop and remove the timeclock
-     * @param target element - the containing div-tag
-     * @return object calculated time as per the _calculateTime method.
-     */
-    _destroyTimeClock: function(target) {
-        target = $(target);
-        if (!target.hasClass(this.markerClassName)) {
-            return;
-        }
-        var inst = $.data(target[0], PROP_NAME);
-        if (inst._timer) {
-            clearTimeout(inst._timer);
-        }
-        target.removeClass(this.markerClassName).html(inst._origHTML);
-        $.removeData(target[0], PROP_NAME);
-        return this._calculateTime(inst);
-    },
+	/**
+	 * Stop and remove the timeclock
+	 * @param target element - the containing div-tag
+	 * @return object calculated time as per the _calculateTime method.
+	 */
+	_destroyTimeClock: function(target) {
+		target = $(target);
+		if (!target.hasClass(this.markerClassName)) {
+			return;
+		}
+		var inst = $.data(target[0], PROP_NAME);
+		if (inst._timer) {
+			clearTimeout(inst._timer);
+		}
+		target.removeClass(this.markerClassName).html(inst._origHTML);
+		$.removeData(target[0], PROP_NAME);
+		return this._calculateTime(inst);
+	},
 
-    /**
-     * Get a setting value, defaulting if necessary.
+	/**
+	 * Get a setting value, defaulting if necessary.
 	 * @param  inst  object - the current settings for this instance
 	 * @param  name  string - the name of the required setting
 	 * @return any - the setting's value or a default if not overridden
-     */
-    _get: function(inst, name) {
-        if (inst.options[name] != null) {
-            return inst.options[name];
-        }
-        else {
-            return $.timeclock._defaults[name];
-        }
-    },
+	 */
+	_get: function(inst, name) {
+		if (inst.options[name] != null) {
+			return inst.options[name];
+		}
+		else {
+			return $.timeclock._defaults[name];
+		}
+	},
 
-    /**
-     * Calculate configuration settings for an instance.
-     * @param inst object - the current settings for this instance
-     */
-    _adjustSettings: function(inst) {
-        var now = new Date();
-        inst._since = now;
-    },
+	/**
+	 * Calculate configuration settings for an instance.
+	 * @param inst object - the current settings for this instance
+	 */
+	_adjustSettings: function(inst) {
+		var now = new Date();
+		if (inst.options.since) {
+			if (inst.options.since.getTime() > now.getTime()) {
+				inst._since = now;
+			}
+			else {
+				inst._since = inst.options.since;
+			}
+		}
+		else {
+			inst._since = new Date();
+		}
+		inst._since = inst._since.setMilliseconds(0);
+	},
 
-    /**
-     * Generate the HTML to display the timeclock widget.
-     * @param inst object - the current settings for this instance
-     * @return string - the new HTML for the timeclock display
-     */
-    _generateHTML: function(inst) {
-        var time = inst._currentTime = this._calculateTime(inst);
-        var twoDigits = function(value) {
+	/**
+	 * Generate the HTML to display the timeclock widget.
+	 * @param inst object - the current settings for this instance
+	 * @return string - the new HTML for the timeclock display
+	 */
+	_generateHTML: function(inst) {
+		var time = inst._currentTime = this._calculateTime(inst);
+		var twoDigits = function(value) {
 			return (value < 10 ? '0' : '') + value;
 		};
-        var container = $("<div/>").addClass("timeclock");
-        $(container).append('<span class="hours">' + time.hours +'</span>');
-        $(container).append('<span class="sep-hours">:</span>');
-        $(container).append('<span class="minutes">' + twoDigits(time.minutes) +'</span>');
-        return container;
-    },
+		var container = $("<div/>").addClass("timeclock");
+		$(container).append('<span class="hours">' + time.hours +'</span>');
+		$(container).append('<span class="sep-hours">:</span>');
+		$(container).append('<span class="minutes">' + twoDigits(time.minutes) +'</span>');
+		return container;
+	},
 
-    /**
-     * Calculate the passed that has passed.
-     * @param inst object - the current settings for this instance.
-     * $return object - the amount of time, split into hours, minutes, seconds and useconds.
-     */
-    _calculateTime: function(inst) {
-        var now = new Date();
-        var msecs = now - inst._since;
-        var time = {
-            hours: 0,
-            minutes: 0,
-            seconds: 0,
-            mseconds: 0
-        }
-        var reduceTime = function(name, nummSecs) {
-            time[name] = Math.floor(msecs / nummSecs);
-            msecs -= time[name] * nummSecs;
-        };
-        reduceTime('hours', 3600000);
-        reduceTime('minutes', 60000);
-        reduceTime('seconds', 1000);
-        reduceTime('mseconds', 1);
-        return time;
-    }
+	/**
+	 * Calculate the passed that has passed.
+	 * @param inst object - the current settings for this instance.
+	 * $return object - the amount of time, split into hours, minutes, seconds and useconds.
+	 */
+	_calculateTime: function(inst) {
+		var now = new Date();
+		var msecs = now - inst._since;
+		var time = {
+			hours: 0,
+			minutes: 0,
+			seconds: 0,
+			mseconds: 0
+		}
+		var reduceTime = function(name, nummSecs) {
+			time[name] = Math.floor(msecs / nummSecs);
+			msecs -= time[name] * nummSecs;
+		};
+		reduceTime('hours', 3600000);
+		reduceTime('minutes', 60000);
+		reduceTime('seconds', 1000);
+		reduceTime('mseconds', 1);
+		return time;
+	}
 
 });
 
